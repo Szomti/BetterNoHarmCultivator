6212146ff6fe0d75581ffc08d34b14e65680aea4
Szomti <piotrpiotr1285@gmail.com>
2024-11-17 14:34:07 +0100

    Clear files

--- obj/Debug/net46/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BetterNoHarmCultivator
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "BetterNoHarmCultivator";
-        public const string PLUGIN_NAME = "Better No-Harm Cultivator";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
